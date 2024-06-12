23d6d794e6062004ce82c79083e9fa59f2082d54
Eduardo Leon <82557141+AmazingE-dot@users.noreply.github.com>
2024-06-11 20:40:58 -0500

    Se descomentan lineas

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -7,9 +7,9 @@ const { host, port, email, password } = enviroment.email
 
 export const transporter = nodemailer.createTransport({
   service: 'hotmail',
-  // host: "smtp-mail.outlook.com",
-  // port: 587,
-  // secure: true,
+  host: "smtp-mail.outlook.com",
+  port: 587,
+  secure: true,
   auth: {
     user: "carlos-eduardo-leon2@hotmail.com",
     pass: "EduardoSan12321.",
